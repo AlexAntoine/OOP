8c3f9e4c724b08cc70bf6dd44a06def63fcb3677
Don-Alex Antoine <dantoine2016@students.curry.edu>
2020-02-20 15:34:47 -0500

    Added Class Diagram

--- OOP_StudentProgram/OOP_StudentProgram/Student.cs
+++ OOP_StudentProgram/OOP_StudentProgram/Student.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_StudentProgram
+{
+    public class Student
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _gender;
+        private string _major;
+
+        public Student()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setFirstName(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void getFirstName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setLastName(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void getLastName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setGender(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void getGender()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setMajor(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void getMajor()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
