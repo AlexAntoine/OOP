9f80850fceafdb25bf84a679a739d4c1f98edc56
Don-Alex Antoine <dantoine2016@students.curry.edu>
2020-02-20 16:23:03 -0500

    created Dates class

---
+++ OOP_StudentProgram/OOP_StudentProgram/Dates.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_StudentProgram
+{
+    public class Dates
+    {
+        private string _day;
+        private string _month;
+        private string _year;
+
+        public Dates()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setDay(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getDay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool setMonth(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void getMonth()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setYear(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getYear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getYear()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
